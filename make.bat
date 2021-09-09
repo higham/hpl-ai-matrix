afde96553d51500eebdd626fb1c3597b8469f2da
Nick Higham <nick.higham@manchester.ac.uk>
2021-09-09 19:48:06 +0100

    Remove make.bat

--- make.bat
+++
@@ -1,6 +0,0 @@
-rem [2021-09-09 Thu 19:42]  Now working on this repo directly as master copy. 
-
-rem copy /u \tex\nopivtest\matlab\hpl_ai_matrix.m 
-rem copy /u \tex\nopivtest\matlab\test_hpl_ai_matrix.m 
-rem copy /u \tex\nopivtest\matlab\fun_based_unit_tests.m
-
