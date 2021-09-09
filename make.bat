f3db0a1e08e07651b0adc3d3e501c62757ac0374
Nick Higham <nick.higham@manchester.ac.uk>
2021-09-09 19:44:34 +0100

    Change readme.

--- make.bat
+++ make.bat
@@ -1,4 +1,6 @@
-copy /u \tex\nopivtest\matlab\hpl_ai_matrix.m 
-copy /u \tex\nopivtest\matlab\test_hpl_ai_matrix.m 
-copy /u \tex\nopivtest\matlab\fun_based_unit_tests.m
+rem [2021-09-09 Thu 19:42]  Now working on this repo directly as master copy. 
+
+rem copy /u \tex\nopivtest\matlab\hpl_ai_matrix.m 
+rem copy /u \tex\nopivtest\matlab\test_hpl_ai_matrix.m 
+rem copy /u \tex\nopivtest\matlab\fun_based_unit_tests.m
 
