549a44c9e4a2b7d9519b63015dc90702959132ab
Nick Higham <nick.higham@manchester.ac.uk>
2021-09-16 14:00:51 +0100

    Update comments in Contnts.m.

--- Contents.m
+++ Contents.m
@@ -1,3 +1,3 @@
 %   HPL-AI-MATRIX (Anymatrix group HPL-AI-MATRIX)
 %   hpl_ai_matrix  - construct n-by-n matrix designed for use in the
-%                    HPL-AI Benchmark.
+%                    HPL-AI Mixed Precision Benchmark.
