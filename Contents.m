02bcc75bdf150cef9b2ca0758f78e2da7b8360ad
Nick Higham <nick.higham@manchester.ac.uk>
2021-09-16 13:54:09 +0100

    Correct function name in Contnts.m.

--- Contents.m
+++ Contents.m
@@ -1,3 +1,3 @@
 %   HPL-AI-MATRIX (Anymatrix group HPL-AI-MATRIX)
-%   hpl-ai-matrix  - construct n-by-n matrix designed for use in the
+%   hpl_ai_matrix  - construct n-by-n matrix designed for use in the
 %                    HPL-AI Benchmark.
